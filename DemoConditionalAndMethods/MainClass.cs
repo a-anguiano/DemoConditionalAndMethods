84079dfbf0ac3758f80b6e6f4f22912ee53314c6
Allison Anguiano <aanguiano@dev-10.com>
2022-03-10 09:51:51 -0600

    Implement method to check if number is within range

---
+++ DemoConditionalAndMethods/MainClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DemoConditionalAndMethods
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            
+        }
+
+        private static bool IsWithinRange(int num, int target, int range)
+        {
+            return (num - target) <= range;
+        }
+    }
+}
