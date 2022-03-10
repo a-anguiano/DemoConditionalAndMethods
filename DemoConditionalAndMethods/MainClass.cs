28d8332e1c825626914e8540a5aa92ed973428c9
Allison Anguiano <aanguiano@dev-10.com>
2022-03-10 10:23:34 -0600

    Ask and you shall receive

--- DemoConditionalAndMethods/MainClass.cs
+++ DemoConditionalAndMethods/MainClass.cs
@@ -6,7 +6,22 @@ namespace DemoConditionalAndMethods
     {
         static void Main()
         {
+            //Ask the user for the initial num
+            Console.WriteLine("What is the initial number? ");
+            int startingNum = int.Parse(Console.ReadLine());
+
+            //Ask the user for the target
+            Console.WriteLine("What is the target? ");
+            int targetNum = int.Parse(Console.ReadLine());
+
+            //Ask the user for the range
+            Console.WriteLine("What is the range?");
+            int range = int.Parse(Console.ReadLine());
+
+            //Write the result by calling IsWithinRange
             
+            Console.WriteLine($"Given a starting number of {startingNum} and a target number of {targetNum}. " +
+                $"It's {IsWithinRange(startingNum, targetNum, range)}");
         }
 
         private static bool IsWithinRange(int num, int target, int range)
